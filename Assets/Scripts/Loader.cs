fd734f24d090a80d9d5a9024a9b30aff5ea5bab4
Balpreet Singh <balpreets10@gmail.com>
2018-10-02 13:09:11 +0530

    Initial Commit
    Environment Made

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public void LoadSingleLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Build");
+    }
+}
